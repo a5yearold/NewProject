2c2d3df8ed5f052a5e860128a26def7748418465
dong <dong.k.woo@likelion.org>
2017-07-13 19:28:24 +1000

    added diff image sizes, plus edit avatar option

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,7 +2,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -30,9 +30,17 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+   version :thumb do
+   process resize_to_fit: [100, 100]
+   end
+
+   version :small do
+   process resize_to_fit: [200, 200]
+   end
+
+   version :medium do
+   process resize_to_fit: [400, 400]
+   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -2,13 +2,13 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAIX7SAHAQL45JHGCA'ENV['aws_access_key'],       # required for editing authentication
-    aws_secret_access_key: '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'ENV['aws_secret_key'],                        # required for editing authetnication
+    aws_access_key_id:     'AKIAILZCQ5MFT7CGZAVA', #ENV['aws_access_key'],       # required for editing authentication
+    aws_secret_access_key: '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',#ENV['aws_secret_key'],                        # required for editing authetnication
     region:                'ap-southeast-2',             # optional, defaults to 'us-east-1' southeast-2 is set to Sydney
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = 'likelion135'                                # required
+  config.fog_directory  = 'likelion1357'                                # required
   config.fog_public     = true                                         # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
